0f22a928993a5277d82884374b2d5b0e1aea2cfa
Nom <Vasapoopkin91@mail.ru>
2021-12-06 18:49:00 +0300

    Start

---
+++ database/migrations/2021_12_06_153859_create_user_data_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserDataTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_data', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->string('img')->default('/img/');
+            $table->string('position');
+            $table->integer('phone');
+            $table->string('address');
+            $table->integer('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_data');
+    }
+}
